129121997fbbcf381f1380e1dd787f628cdc86d6
uberset <uberset@users.noreply.github.com>
2015-11-09 15:32:13 +0100

    parsing

---
+++ src/main/scala/uberset/basic_compiler/Parser.scala
@@ -0,0 +1,69 @@
+package uberset.basic_compiler
+
+import java.io.{BufferedReader, StringReader}
+
+/*
+  Author: uberset
+  Date: 2015-11-09
+  Licence: GPL v2
+
+*/
+
+/*
+grammar:
+  program = 'PRINT' string ;
+  string = '"' string-char* '"' ;
+  string-char = ? any character except '"' and newline> ? ;
+*/
+
+case class Program(print: Print)
+case class Print(string: String)
+
+object Parser {
+
+    def parse(text: String): Program = parse(new BufferedReader(new StringReader(text)))
+
+    def parse(in: BufferedReader): Program = program(in)
+
+    def program(in: BufferedReader): Program = {
+        val s = in.readLine()
+        val p: Print = stmPrint(s)
+        Program(p)
+    }
+
+    val PRINT = "PRINT"
+
+    def stmPrint(s: String): Print = {
+        val s1 = require(PRINT, s)
+        val (str, rest) = string(s1)
+        if(!rest.isEmpty) fail(s"Unparsed text after statement: $rest")
+        Print(str)
+    }
+
+    def string(s: String): (String, String) = {
+        val s1 = require('"', s)
+        val str = s1.takeWhile(_ != '\"')
+        val end = s1.substring(str.length)
+        val rest = require('"', end)
+        (str, rest)
+    }
+
+    def require(c: Char, s: String): String = {
+        if(!s.isEmpty && s.charAt(0)==c)
+            s.substring(1)
+        else
+            fail(s"'$c' is required at: '$s'")
+    }
+
+    def require(text: String, s: String): String = {
+        if(s.startsWith(text))
+            s.substring(text.length)
+        else
+            fail(s"'$text' is required at: '$s'")
+    }
+
+    def fail(msg: String): Null = {
+        throw new Exception(msg)
+    }
+
+}
---
+++ src/test/scala/uberset/basic_compiler/TestParser.scala
@@ -0,0 +1,37 @@
+package uberset.basic_compiler
+
+/*
+  Author: uberset
+  Date: 2015-11-09
+  Licence: GPL v2
+
+*/
+
+object TestParser {
+
+    def main(args: Array[String]): Unit = {
+        val results = Seq(
+            test("""PRINT"Hello World!"""", Program(Print("Hello World!")))
+        )
+        val tests = results.size
+        val passed = results.filter(identity).size
+        val failed = tests - passed
+        if(failed>0)
+            println(s"$failed of $tests tests failed.")
+        else
+            println(s"All $tests tests passed.")
+    }
+
+    def test(text: String, tree: Program): Boolean = {
+        val prog = Parser.parse(text)
+        assertEquals(prog, tree)
+    }
+
+    def assertEquals(value: AnyRef, expected: AnyRef): Boolean = {
+        val condition = value == expected
+        if(!condition) {
+            println(s"value: $value does not equals expected: $expected")
+        }
+        condition
+    }
+}
